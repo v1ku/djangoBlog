2fc316702e3915f0f81b184b4e7e7f63d4ed51f7
v1ku <vishnu.a.nair1995@gmail.com>
2022-11-20 13:38:35 +0530

    formatting

--- ablog/newBlog/models.py
+++ ablog/newBlog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
+    title_tag = models.CharField(max_length=255, default='Title')
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)

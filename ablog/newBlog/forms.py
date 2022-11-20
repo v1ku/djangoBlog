1c2d99181ad8b618c044941af6748ae54b099c8a
v1ku <vishnu.a.nair1995@gmail.com>
2022-11-20 14:13:54 +0530

    styling the input form

---
+++ ablog/newBlog/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'content', 'author')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+        }

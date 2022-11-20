9c9e4170ff43b9732ee424ce73fa30689caf45ef
v1ku <vishnu.a.nair1995@gmail.com>
2022-11-20 14:30:33 +0530

    added update form

--- ablog/newBlog/views.py
+++ ablog/newBlog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm,EditForm
 # Create your views here.
 #def home(request):
 #    return render(request, 'home.html', {})
@@ -23,5 +23,5 @@ class AddPostView(CreateView):
 
 class UpdatePostView(UpdateView):
     model = Post
+    form_class = EditForm
     template_name = 'update_post.html'
-    fields = ['title','title_tag','content']

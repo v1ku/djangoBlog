05e4368ed78ec4f1ca8d46c5272ba74d50b64c24
v1ku <vishnu.a.nair1995@gmail.com>
2022-11-20 13:05:36 +0530

    missing import

--- ablog/newBlog/urls.py
+++ ablog/newBlog/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 #from . import views
-from .views import HomeView
+from .views import HomeView, ArticleDetailView
 urlpatterns = [
     #path('', views.home, name='home'),
     path('', HomeView.as_view(), name='home'),

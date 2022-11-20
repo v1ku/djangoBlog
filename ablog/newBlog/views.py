efd2ec79a676f3390745456962fe9737e68199af
v1ku <vishnu.a.nair1995@gmail.com>
2022-11-20 12:58:44 +0530

    adding detailed views

--- ablog/newBlog/views.py
+++ ablog/newBlog/views.py
@@ -9,3 +9,7 @@ class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     ordering = ['-date_posted']
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
